dc111615781e7b6beff2415ff0eb00168062dcd9
ijmari <issamjmari098@gmail.com>
2021-12-15 20:47:16 +0100

    New norminette stuff

--- so_long_utils.c
+++ so_long_utils.c
@@ -6,7 +6,7 @@
 /*   By: ijmari <ijmari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 17:22:43 by ijmari            #+#    #+#             */
-/*   Updated: 2021/12/15 20:42:38 by ijmari           ###   ########.fr       */
+/*   Updated: 2021/12/15 20:46:10 by ijmari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_draw_elem (int x, int y, void *path, t_img i)
 	mlx_put_image_to_window (i.mlx, i.win, i.img, x * 50, y * 50);
 }
 
-void	wex (char **all_lines, t_img i)
+t_player	wepx (char **all_lines, t_img i, t_player test)
 {
 	if (all_lines[y][x] == '1')
 		ft_draw_elem (x, y, "./wall.xpm", i);
@@ -29,6 +29,14 @@ void	wex (char **all_lines, t_img i)
 		ft_draw_elem (x, y, "./blueone.xpm", i);
 	else if (all_lines[y][x] == 'E')
 		ft_draw_elem (x, y, "./door.xpm", i);
+	else if (all_lines[y][x] == 'P')
+	{
+		ft_draw_elem (x, y, "./blueone.xpm", i);
+		ft_draw_elem (x, y, "./player.xpm", i);
+		test.posx = x;
+		test.posy = y;
+	}
+	return (test);
 }
 
 t_player	put_elems (char **all_lines, t_player test, t_img i)
@@ -45,15 +53,8 @@ t_player	put_elems (char **all_lines, t_player test, t_img i)
 				ft_draw_elem (x, y, "./Ninja.xpm", i);
 				test.coin++;
 			}
-			else if (all_lines[y][x] == 'P')
-			{
-				ft_draw_elem (x, y, "./blueone.xpm", i);
-				ft_draw_elem (x, y, "./player.xpm", i);
-				test.posx = x;
-				test.posy = y;
-			}
 			else
-				wex (all_lines, i);
+				test = wepx (all_lines, i, test);
 			x++;
 		}
 		y++;
